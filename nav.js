df248db81c983de97919c8a9428f7555c969b70c
kjgilchrist <93557317+kjgilchrist@users.noreply.github.com>
2021-11-27 17:13:44 -0700

    Create nav.js

---
+++ nav.js
@@ -0,0 +1,47 @@
+// Current Page - I see what it is trying to do, but...
+/*
+let currentLinks = document.querySelectorAll('a[href="'+document.URL+'"]');
+    currentLinks.for(currentLink) {
+        link.className += ' .current-link';
+    }
+*/
+
+// Sticky Navigation Menu & Up Button
+//let nav = document.querySelector("nav");
+let scrollBtn = document.querySelector(".scroll-button a");
+//console.log(scrollBtn);
+//let val;
+window.onscroll = function() {
+  if(document.documentElement.scrollTop > 20){
+    //nav.classList.add("sticky");
+    scrollBtn.style.display = "block";
+  }else{
+    //nav.classList.remove("sticky");
+    scrollBtn.style.display = "none";
+  }
+}
+
+// Side Navigation Menu
+let navBar = document.querySelector(".navbar .menu");
+let menuBtn = document.querySelector(".menu-btn");
+let cancelBtn = document.querySelector(".cancel-btn");
+menuBtn.onclick = function(){
+  navBar.classList.add("active");
+  menuBtn.style.opacity = "0";
+  menuBtn.style.pointerEvents = "none";
+}
+cancelBtn.onclick = function(){
+  navBar.classList.remove("active");
+  menuBtn.style.opacity = "1";
+  menuBtn.style.pointerEvents = "auto";
+}
+
+// Side Navigation Bar Close
+let navLinks = document.querySelectorAll(".menu li a");
+for (var i = 0; i < navLinks.length; i++) {
+  navLinks[i].addEventListener("click" , function() {
+    navBar.classList.remove("active");
+    menuBtn.style.opacity = "1";
+    menuBtn.style.pointerEvents = "auto";
+  });
+}
